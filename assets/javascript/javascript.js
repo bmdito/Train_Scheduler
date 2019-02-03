dcfa98131ed9b0496b1543dcbab48f8aa6c4af0c
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-02-02 21:34:37 -0800

    added 'remove' button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -18,25 +18,35 @@
   var frequency = 0;
 
 
-
+// takes entered values and pushes them to firebase if all fields filled
 $("#submit").on("click", function(event){
     event.preventDefault();
 
-    var trainName = $("#train-imput").val().trim();
-    var destination = $("#destination-imput").val().trim();
-    var startTime = $("#start-imput").val().trim();
-    var frequency = $("#frequency-imput").val().trim();
+    if($("#train-imput").val().trim()===""||
+    $("#destination-imput").val().trim()===""||
+    $("#start-imput").val().trim()===""||
+    $("#frequency-imput").val().trim()===""){
+        alert("missing");
+    } else {
 
-    database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        startTime: startTime,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-    });
+        
+        var trainName = $("#train-imput").val().trim();
+        var destination = $("#destination-imput").val().trim();
+        var startTime = $("#start-imput").val().trim();
+        var frequency = $("#frequency-imput").val().trim();
+        
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            startTime: startTime,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+            
+        });
+    }
 });
 
+// adds information from firebase to populate new row
 database.ref().on(
     "child_added", function(childSnapShot){
         console.log(childSnapShot.val().trainName);
@@ -45,6 +55,8 @@ database.ref().on(
         var destination = childSnapShot.val().destination;
         var startTime = childSnapShot.val().startTime;
         var frequency = childSnapShot.val().frequency;
+        key=childSnapShot.key;
+        console.log(childSnapShot)
 
         var firstTrain = startTime;
 
@@ -57,17 +69,23 @@ database.ref().on(
         
 
         var newRow = $("<tr>");
+
+        newRow.attr("data-key");
+        // newRow.addClass("the-row");
         newRow.append("<td>"+trainName+"</td>");
         newRow.append("<td>"+childSnapShot.val().destination+"</td>");
         newRow.append("<td class='text-center'>"+frequency+"</td>");
         newRow.append("<td class='text-center'>"+theArrival+"</td>");
         newRow.append("<td class='text-center'>"+toArrival+"</td>");
-        newRow.append("<td class='text-center'>"+"<button class='btn btn-danger' id='remove'>"+"remove"+"</button>"+"</td>");
+        newRow.append("<td class='text-center'>"+"<button class='the-row btn btn-danger' data-key='"+key+"'>"+"remove"+"</button>"+"</td>");
 
         $("#train-table-rows").append(newRow);
     }
 );
 
-$("body").on("click", "#remove", function(){
+$("body").on("click", ".the-row", function(){
     console.log("remove clicked");
+    keyref = $(this).attr("data-key");
+  database.ref().child(keyref).remove();
+  window.location.reload();
 })
